a983074cf053838c48dc14b7017a010007602a6e
Wahidin <a17wahidin@gmail.com>
2019-11-18 00:42:56 +0700

    copas
    oi

---
+++ IcelandWahidin/IcelandWahidin/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IcelandWahidin.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string HeroColor { get; set; }
+    }
+}
---
+++ IcelandWahidin/IcelandWahidin/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmHelpers;
+
+namespace IcelandWahidin.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public IList<ProductViewModel> Products { get; set; }
+        public MainViewModel()
+        {
+            Products = new ObservableRangeCollection<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Name = "Sky Blue",
+                    HeroColor = "#95C9F7",
+                    ImageUrl = "mossGreen",
+                    Price = 12,
+                    IsFeatured = true
+                },
+                new ProductViewModel()
+                {
+                    Name = "Yellow Sun",
+                    HeroColor = "#FFCA81",
+                    ImageUrl = "mossGreen",
+                    Price = 17,
+                    IsFeatured = false
+                },
+                new ProductViewModel()
+                {
+                    Name = "Lavender",
+                    HeroColor = "#D69EFC",
+                    ImageUrl = "mossGreen",
+                    Price = 19,
+                    IsFeatured = false
+                },
+                new ProductViewModel()
+                {
+                    Name = "Green Life",
+                    HeroColor = "#74D69E",
+                    ImageUrl = "mossGreen",
+                    Price = 14,
+                    IsFeatured = true
+                },
+            };
+        }
+    }
+}
